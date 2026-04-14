79f9124b24aeb74dcd92c526779fb3195882e3dd
Quin Nhu <quynhnhu.pt99@gmail.com>
2026-04-14 14:20:12 +0700

    ignore download file

--- tests/the-internet/download-file.spec.ts
+++ tests/the-internet/download-file.spec.ts
@@ -1,35 +1,35 @@
-import { test, expect } from '@playwright/test';
-import path from 'path';
-import fs from 'fs';
+// import { test, expect } from '@playwright/test';
+// import path from 'path';
+// import fs from 'fs';
 
-test('download a file', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/download');
+// test('download a file', async ({ page }) => {
+//   await page.goto('https://the-internet.herokuapp.com/download');
 
-  const [download] = await Promise.all([
-    page.waitForEvent('download'),
-    page.getByRole('link', { name: 'Image.PNG', exact: true }).click()
-  ]);
-  const filePath = path.join(__dirname, '../resources/download', download.suggestedFilename());
-  await download.saveAs(filePath);
+//   const [download] = await Promise.all([
+//     page.waitForEvent('download'),
+//     page.getByRole('link', { name: 'Image.PNG', exact: true }).click()
+//   ]);
+//   const filePath = path.join(__dirname, '../resources/download', download.suggestedFilename());
+//   await download.saveAs(filePath);
   
-  expect(fs.existsSync(filePath)).toBeTruthy();
-  expect(filePath).toContain('Image.PNG');
-});
+//   expect(fs.existsSync(filePath)).toBeTruthy();
+//   expect(filePath).toContain('Image.PNG');
+// });
 
-test('download multiple files', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/download');
-  const fileName = ['testfile1.pdf', 'Jpeg_with_exif.jpeg', 'TestData.xlsx'];
+// test('download multiple files', async ({ page }) => {
+//   await page.goto('https://the-internet.herokuapp.com/download');
+//   const fileName = ['testfile1.pdf', 'Jpeg_with_exif.jpeg', 'TestData.xlsx'];
 
-  for (const name of fileName) {
-    const [download] = await Promise.all([
-      page.waitForEvent('download'),
-      page.getByRole('link', { name: name, exact: true }).click()
-    ]);
+//   for (const name of fileName) {
+//     const [download] = await Promise.all([
+//       page.waitForEvent('download'),
+//       page.getByRole('link', { name: name, exact: true }).click()
+//     ]);
 
-    const filePath = path.join(__dirname, '../resources/download', download.suggestedFilename());
-    await download.saveAs(filePath);
+//     const filePath = path.join(__dirname, '../resources/download', download.suggestedFilename());
+//     await download.saveAs(filePath);
 
-    expect(fs.existsSync(filePath)).toBeTruthy();
-    expect(filePath).toContain(name);
-  }
-});
+//     expect(fs.existsSync(filePath)).toBeTruthy();
+//     expect(filePath).toContain(name);
+//   }
+// });
